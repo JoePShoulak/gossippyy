c1d9af8bdc5b86ae50440531c911d6904efba81a
Kayla Justice <kaylarjustice@gmail.com>
2022-10-27 10:33:56 -0500

    Building out Post.js model

--- models/User.js
+++ models/User.js
@@ -2,12 +2,14 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+// create User model
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
+// create fields/coumns for Post model
 User.init(
   {
     id: {
