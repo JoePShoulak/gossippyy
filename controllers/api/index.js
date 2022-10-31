89401847be67cde2c837fb5acb1a41708d1dfd92
tarekmn <nytarek@gmail.com>
2022-10-31 11:19:13 -0500

    adding comment button

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const homeRoutes = require("../homeRoutes");
+const commentRoutes = require("../api/commentRoutes");
 
 router.use("/", homeRoutes);
 router.use("/users", userRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
