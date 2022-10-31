27bac3a3af350b3235e53b4c18967269e5bb23f0
Joe P Shoulak <joe@squarecompass.io>
2022-10-31 11:49:46 -0500

    whitespace

--- models/index.js
+++ models/index.js
@@ -1,38 +1,38 @@
 const User = require("./User");
 const Post = require("./Post");
-const Comment = require('./Comment');
+const Comment = require("./Comment");
 
 // a User has many Posts
 User.hasMany(Post, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // a User has many Comments
 User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // a Post belongs to a User
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 // a Comment belongs to a User
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 // a Post has many Comments
 Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE',
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
 });
 
 // A Comment belongs to a Post
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
+  foreignKey: "post_id",
 });
 
 module.exports = { User, Post, Comment };
