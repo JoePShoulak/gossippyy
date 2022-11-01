72576d0983ff7f906d209da2e008a6056770bdfd
Joe P Shoulak <joe@squarecompass.io>
2022-11-01 12:36:26 -0500

    fixed missing follow script

--- public/js/follow.js
+++ public/js/follow.js
@@ -1,14 +1,17 @@
 const follow = async (event) => {
+  console.log("follow button clicked");
   event.preventDefault();
 
   try {
     const id = event.target.dataset.id;
 
+    console.log(id);
+
     await fetch(`/api/users/follow/${id}`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     });
-    location.reload();
+    // location.reload();
   } catch (err) {
     console.log(err);
   }
