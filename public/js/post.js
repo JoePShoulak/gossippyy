c5b68aab2428ece812ab2a3f0b9975223776ae06
tarekmn <nytarek@gmail.com>
2022-11-01 13:45:08 -0500

    sync

--- public/js/post.js
+++ public/js/post.js
@@ -1,15 +1,17 @@
 const createPost = async (event, i) => {
   event.preventDefault();
 
-  try {
-    const content = document.querySelector("#post-content").value;
+  const content = document.getElementById("post-content").value;
 
+  console.log(content);
+
+  try {
     const response = await fetch("/api/post", {
       method: "POST",
       body: JSON.stringify({ content }),
       headers: { "Content-Type": "application/json" },
     });
-    location.reload();
+    // location.reload();
   } catch (err) {
     console.log(err);
   }
